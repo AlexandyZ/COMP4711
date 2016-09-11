5ea55220087f5218ed27f166fc4ddb85450845fc
Alex Zhang <zyf1014@hotmail.com>
2016-09-11 00:30:32 -0700

    update comments

--- PhpProject1/Student.php
+++ PhpProject1/Student.php
@@ -6,21 +6,39 @@
  */
 class Student {
     
-    function _construct() {
+    /**
+     * Student constructor to initialize a student's properties.
+     * @param string $this->surname    A string of student surname
+     * @param string $this->first_name A string of student firstname
+     * @param array $this->emails      An associative array of student emails
+     * @param array $this->grades      An associative array of student grades
+     */
+    function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
     
+    /**
+     * Add email address in the array of emails
+     * @param string $which    A string of email type
+     * @param string $address  A string of email address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
-
+    /**
+     * Add grade in the array of grades
+     * @param float $grade  An float of grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
-
+    /**
+     * Caculate the average of grades for each student
+     * @return float The average of grades
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value){
@@ -29,7 +47,10 @@ class Student {
         return $total / count($this->grades);
     }
     
-    /* format display */
+    /**
+     * Format display
+     * @return string The formatting student information
+     */
     function toString() {
         $result = $this->first_name . " " . $this->surname . "\t";
         $result .= ' (' . $this->average() . ")\n";
